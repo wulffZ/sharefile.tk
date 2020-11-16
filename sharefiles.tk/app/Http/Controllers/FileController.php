39fbf2a6806e3fba920e239020f644805dae35bd
Matthijs van Zetten <mattje74@gmail.com>
2020-11-16 23:46:10 +0100

    Soft deleted implemented

--- sharefiles.tk/app/Http/Controllers/FileController.php
+++ sharefiles.tk/app/Http/Controllers/FileController.php
@@ -18,32 +18,32 @@ class  FIleController extends Controller
 
     public function videos()
     {
-        $data = File::join('users', 'users.id', '=', 'file.user_id')->select('file.*', 'users.username')->where('type', 'video')->orderBy('created_at', 'desc')->simplePaginate(6);
+        $data = File::join('users', 'users.id', '=', 'file.user_id')->select('file.*', 'users.username')->where('type', 'video')->where('soft_delete', 'false')->orderBy('created_at', 'desc')->simplePaginate(6);
         $video = VideoResource::collection($data);
         return view('fileViews.videos', compact('video', 'data'));
     }
 
     public function games()
     {
-        $data = File::join('users', 'users.id', '=', 'file.user_id')->select('file.*', 'users.username')->where('type', 'game')->orderBy('created_at', 'desc')->get();
+        $data = File::join('users', 'users.id', '=', 'file.user_id')->select('file.*', 'users.username')->where('type', 'game')->where('soft_delete', 'false')->orderBy('created_at', 'desc')->get();
         return view('fileViews.games', compact('data'));
     }
 
     public function images()
     {
-        $data = File::join('users', 'users.id', '=', 'file.user_id')->select('file.*', 'users.username')->where('type', 'image')->orderBy('created_at', 'desc')->get();
+        $data = File::join('users', 'users.id', '=', 'file.user_id')->select('file.*', 'users.username')->where('type', 'image')->where('soft_delete', 'false')->orderBy('created_at', 'desc')->get();
         return view('fileViews.images', compact('data'));
     }
 
     public function other()
     {
-        $data = File::join('users', 'users.id', '=', 'file.user_id')->select('file.*', 'users.username')->where('type', 'other')->orderBy('created_at', 'desc')->get();
+        $data = File::join('users', 'users.id', '=', 'file.user_id')->select('file.*', 'users.username')->where('type', 'other')->where('soft_delete', 'false')->orderBy('created_at', 'desc')->get();
         return view('fileViews.other', compact('data'));
     }
 
     public function music()
     {
-        $data = File::join('users', 'users.id', '=', 'file.user_id')->select('file.*', 'users.username')->where('type', 'music')->orderBy('created_at', 'desc')->get();
+        $data = File::join('users', 'users.id', '=', 'file.user_id')->select('file.*', 'users.username')->where('type', 'music')->where('soft_delete', 'false')->orderBy('created_at', 'desc')->get();
         return view('fileViews.music', compact('data'));
     }
 
@@ -56,7 +56,7 @@ class  FIleController extends Controller
             return redirect('/login');
         }
 
-        $file = File::findOrFail($id);
+        $file = File::where('id', $id)->where('soft_delete', 'false')->first();
 
         switch ($file->type) {
             case "image":
@@ -77,7 +77,7 @@ class  FIleController extends Controller
         $file = File::findOrFail($id);
         try {
             if (Auth::user()->id == $file->user_id || Auth::user()->role == "admin") {
-                $file->delete();
+                $file->soft_delete = true;
             } else {
                 return "You do not have the permissions to perform this action";
             }
@@ -100,12 +100,4 @@ class  FIleController extends Controller
     {
         return view('fileViews.create');
     }
-
-    public function playVideo($id)
-    {
-        $file = File::findOrFail($id);
-        return view('fileViews.play', compact('file'));
-    }
-
-
 }
